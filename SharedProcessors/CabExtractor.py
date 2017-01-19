a746a5fa62d30ca04c610840205d9383d37c8b44
jeremylarose <jeremy.larose@protonmail.com>
2017-01-18 22:39:39 -0500

    Test

--- SharedProcessors/CabExtractor.py
+++ SharedProcessors/CabExtractor.py
@@ -63,7 +63,7 @@ class CabExtractor(Processor):
         default_path = os.path.join(RECIPE_CACHE_DIR, NAME)
         cab_extract_path = self.env.get('cab_extract_path', default_path)
         purge_destination = self.env.get('purge_destination', False)
-        cabextract = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'CabExtractor/cabextract')
+#        cabextract = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'CabExtractor/cabextract')
 
         if os.path.exists(cab_extract_path) and purge_destination == False:
             sys.exit(0)
