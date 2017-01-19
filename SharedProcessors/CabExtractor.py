847a786e08e871b20841bd9659e3d2bdfdc3aa80
jeremylarose <jeremy.larose@protonmail.com>
2017-01-18 22:35:33 -0500

    Undo

--- SharedProcessors/CabExtractor.py
+++ SharedProcessors/CabExtractor.py
@@ -64,7 +64,6 @@ class CabExtractor(Processor):
         cab_extract_path = self.env.get('cab_extract_path', default_path)
         purge_destination = self.env.get('purge_destination', False)
         cabextract = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'CabExtractor/cabextract')
-        cabextract_local = os.path.join(RECIPE_CACHE_DIR, 'downloads', 'cabextract')
 
         if os.path.exists(cab_extract_path) and purge_destination == False:
             sys.exit(0)
@@ -81,9 +80,7 @@ class CabExtractor(Processor):
             self.output('cab_path does not have a cab or CAB extension')
             sys.exit(1)
 
-        shutil.copyfile(cabextract, cabextract_local)
-
-        cmd = [cabextract_local, cab_path, '-d', cab_extract_path]
+        cmd = [cabextract, cab_path, '-d', cab_extract_path]
         proc = subprocess.Popen(cmd,  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout, stderr) = proc.communicate()
 
